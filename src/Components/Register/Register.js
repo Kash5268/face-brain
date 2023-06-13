9b677481d968daf9431e2a98ec469c578929be0f
Kash5268 <kalmahanbetalihan@gmail.com>
2023-06-13 15:43:49 +0600

    change in fetch

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onSubmitRegister = () => {
-    fetch("http://localhost:3000/register", {
+    fetch("https://https://face-brain-api-63vo.onrender.com//register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("http://localhost:3000/signin", {
+    fetch("https://https://face-brain-api-63vo.onrender.com//signin", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
